b948194f8518b3c48da914298cfd916df7580507
Shenal Akalanka <shenalakalanka513@gmail.com>
2023-02-20 11:46:56 +0530

    update

--- src/Components/SignIn/SignIn.jsx
+++ src/Components/SignIn/SignIn.jsx
@@ -1,32 +1,43 @@
-import React from 'react'
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
 
-import './SignIn.css'
 const SignIn = () => {
   return (
-    <div className="signin-container">
-      
-        <Form className='signin-form'>
-        <h2 className='title'>Welcome to GPA Calculator</h2>
-        <FloatingLabel
-        controlId="floatingInput"
-        label="Email address"
-        className="mb-3"
-      >
-        <Form.Control type="email" placeholder="name@example.com" />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingPassword" label="Password">
-        <Form.Control type="password" placeholder="Password" />
-      </FloatingLabel>
+    <div className="bg-light">
+      <div className="container">
+        <Row className="mt-5">
+          <Col lg={4} className="bg-light m-auto">
+            <h2 className="text-center pt-3">SignUp Now</h2>
+            <p className="text-center text-muted lead">
+              It's free and take a minute
+            </p>
 
-      <a href='#' className='forget-psw'>Forget password</a>
-      <hr className='line'/>
-        </Form>
+            <Form action="#">
+           <div className="input-group">
+           <FloatingLabel
+                controlId="floatingInput"
+                label="Email address"
+                className="mb-3"
+              >
+                <Form.Control type="email" placeholder="name@example.com" />
+              </FloatingLabel>
+           </div>
 
-        
+<div className="input-group">
+<FloatingLabel controlId="floatingPassword" label="Password">
+                <Form.Control type="password" placeholder="Password" />
+              </FloatingLabel>
+</div>
+              
+            </Form>
+          </Col>
+        </Row>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
